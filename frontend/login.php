53f549a16616c56692f9a877685fd826dad5cd22
Feysal <feisalabdul25@gmail.com>
2026-02-23 02:20:56 -0500

    Fixed an issue where authentication message was not updating

--- frontend/login.php
+++ frontend/login.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_WARNING);
 
 require_once('../backend/rabbitMQLib.inc');
 
@@ -21,10 +21,14 @@ switch ($request["type"])
 	case "login":
 		$response = $client->send_request($request);
 		if (!isset($response)) {
-		echo json_encode(["status" => false, "message" => "RabbitMQ Returned Nothing"]);
+		echo json_encode(["status" => false, "message" => "RabbitMQ return nothing"]);
 		exit(0);
 		}
-		echo json_encode($response);
+		if (is_array($response)) {
+			echo json_encode($response);
+		} else {
+			echo $response;
+		}
 		exit(0);
 	break;
 }
