436217e7df91ea8a44f5397429be048c4fad0798
Feysal <feisalabdul25@gmail.com>
2026-03-09 19:04:41 -0400

    added remove from library button

--- backend/testRabbitMQServer.php
+++ backend/testRabbitMQServer.php
@@ -294,7 +294,39 @@ case "addToLibrary":
 		}
 	}
 	return array("returnCode" => '0', 'message' => "Error While updating library.(DB)");
+//remove game goes here
+case "removeGame":
+	global $pdo;
+	$sessionKey = $request['session_key'];
+	$gameId = $request['game_id'];
+	$status = $request['status'];
 
+	$getUser = $pdo->prepare("SELECT userid FROM sessions WHERE session_id = ?");
+	$getUser->execute([$sessionKey]);
+	$userR = $getUser->fetch(PDO::FETCH_ASSOC);
+	if (!$userR) {
+	return array("returnCode" => '0', 'message' => "Login again please");
+	}
+
+	$userId = $userR['userid'];
+
+	$check = $pdo->prepare("SELECT id FROM user_library WHERE user_id = ? AND game_id = ?");
+	$check->execute([$userId, $gameId]);
+
+	if ($check->rowCount() > 0) {
+		$update = $pdo->prepare("REMOVE from  user_library WHERE user_id = ? AND game_id = ?");
+	if ($update->execute([$status, $userId, $gameId])) {
+		return array("returnCode" => '1', 'message' => "Library Updated!");
+	}
+	}
+	else {
+		$insert = $pdo->prepare("INSERT INTO user_library (user_id, game_id, status) VALUES (?, ?, ?)");
+		if ($insert->execute([$userId, $gameId, $status])) {
+		return array ("returnCode" => '1', 'message' => "Added to your library!");
+		}
+	}
+	return array("returnCode" => '0', 'message' => "Error While updating library.(DB)");
+	//remove game ends here
 case "get_user_library":
 	global $pdo;
 	$sessionKey = $request['session_key'];
---
+++ frontend/removeGame.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+require_once('../backend/path.inc');
+require_once('../backend/get_host_info.inc');
+require_once('../backend/rabbitMQLib.inc');
+
+if (!isset($_SESSION['logged_in']) && $_SESSION['logged_in'] !== true) {
+        header("Location: index.php");
+        exit(0);
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['game_id'])) {
+	$client = new rabbitMQClient("../backend/testRabbitMQ.ini", "testServer");
+	$request = array();
+	$request['type'] = "removeGame";
+	$request['session_key'] = $_SESSION['session_key'];
+	$request['game_id'] = $_POST['game_id'];
+	$request['status'] = $_POST['status'];
+
+	$response = $client->send_request($request);
+
+	//here is just a message to let user know if game was added to their lib. 
+
+	if (isset($response['returnCode']) && $response['returnCode'] == '1') {
+		$_SESSION['library_mess'] = $response['message'];
+		$_SESSION['library_mess_type'] = "success";
+	}
+	else {
+	$_SESSION['library_mess'] = $response['message'] ??"Failed to remove Game";
+                $_SESSION['library_mess_type'] = "danger";
+	}
+	header("Location: game_profile.php?id=" . $_POST['game_id']);
+	exit();
+}
+else {
+	header("Location: search_results.php");
+	exit();
+}
+?>
