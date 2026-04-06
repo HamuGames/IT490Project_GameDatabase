2bc77c5064b736e10993f3b766ea0e26c59cd7bb
Feysal <feisalabdul25@gmail.com>
2026-04-05 20:35:31 -0400

    script to package current MAIN repo state to a file

---
+++ deployer/package.php
@@ -0,0 +1,18 @@
+<?php
+
+if ($argc < 2) {
+	die("You must provide a version number!");
+}
+
+$version = $argv[1];
+$projectFolder = "/home/fabdul25/git/IT490_Project";
+$versionFolder = "/home/fabdul25/deployment/versions";
+$fileName = "$versionFolder/v$version.tar.gz";
+
+echo "New version v$version in process...";
+exec("cd $projectFolder && git checkout main && git pull origin main");
+exec("tar -czf $fileName -C $projectFolder .");
+
+echo "New Version v$version has been created in $versionFolder";
+
+?>
