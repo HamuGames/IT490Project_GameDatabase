48d44642448462bb8a52a7215fdb461743d860e0
Feysal <feisalabdul25@gmail.com>
2026-04-05 20:48:00 -0400

    visual fix with \n

--- deployer/package.php
+++ deployer/package.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($argc < 2) {
-	die("You must provide a version number!");
+	die("You must provide a version number!\n");
 }
 
 $version = $argv[1];
@@ -12,7 +12,8 @@ $fileName = "$versionFolder/v$version.tar.gz";
 echo "New version v$version in process...";
 exec("cd $projectFolder && git checkout main && git pull origin main");
 exec("tar -czf $fileName -C $projectFolder .");
+exec("cd $projectFolder && git checkout Deployment");
 
-echo "New Version v$version has been created in $versionFolder";
+echo "New Version v$version has been created in $versionFolder\n";
 
 ?>
