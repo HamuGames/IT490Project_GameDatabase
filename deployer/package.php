9bb8874ea1a98da29ba5e896d44b5acb63b1a995
Feysal <feisalabdul25@gmail.com>
2026-04-12 12:17:43 -0400

    changed to automatic version creation. started sql table insertions

--- deployer/package.php
+++ deployer/package.php
@@ -1,15 +1,21 @@
 <?php
 
-if ($argc < 2) {
-	die("You must provide a version number!\n");
-}
-
-$version = $argv[1];
 $projectFolder = "/home/fabdul25/git/IT490_Project";
 $versionFolder = "/home/fabdul25/deployment/versions";
-$fileName = "$versionFolder/v$version.tar.gz";
 
-echo "New version v$version in process...";
+$deploy = parse_ini_file(__DIR__ . '/deployer.ini', true)['database'];
+$db = new mysqli($deploy['host'], $deploy['user'], $deploy['pass'], $deploy['name']);
+
+$result = $db->query("SELECT version FROM packages ORDER BY deploy_date DESC LIMIT 1");
+$version = "v1.0.0";
+
+if ($row = $result->fetch_assoc()) {
+	$num = explode('.', ltrim($row['version'], 'v'));
+	$nums[2]++;
+	$version = "v" . implode('.', $num);
+}
+$fileName = "$versionFolder/$version.tar.gz";
+echo "New version $version in process...";
 exec("cd $projectFolder && git checkout main && git pull origin main");
 exec("tar -czf $fileName -C $projectFolder .");
 exec("cd $projectFolder && git checkout Deployment");
